a5558849873809bc649cb1bec00eb30517e9a5bd
Caroline Jeffra <caroline.jeffra@gmail.com>
2023-10-07 18:23:44 +0200

    Group#initialize, #read?, #read!, all tests pass

---
+++ app/models/group.rb
@@ -0,0 +1,20 @@
+class Group
+  attr_accessor :id
+  attr_reader :book, :theme, :bookworm
+
+  def initialize(attrs = {})
+    @id = attrs[:id]
+    @read = attrs[:read] || false
+    @book = attrs[:book]
+    @theme = attrs[:theme]
+    @bookworm = attrs[:bookworm]
+  end
+
+  def read?
+    return @read
+  end
+
+  def read!
+    @read = true
+  end
+end
